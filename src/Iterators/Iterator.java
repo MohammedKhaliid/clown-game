86359e656cc201fcc05e6d01a42d2cb19f03eefe
Eiad Alaa <eiad.alaa.nouh@gmail.com>
2023-12-21 08:27:58 +0200

    iterator commit

--- src/Iterators/Iterator.java
+++ src/Iterators/Iterator.java
@@ -1,4 +1,9 @@
 package Iterators;
-
+import Characters.Mushroom;
 public interface Iterator {
+
+    void add(Mushroom mushroom);
+    boolean lastThreeMatches();
+
+    Mushroom lastMushroom();
 }
--- src/Iterators/LeftHand.java
+++ src/Iterators/LeftHand.java
@@ -1,6 +1,34 @@
 package Iterators;
 
+import Characters.Mushroom;
 import Iterators.Iterator;
 
+import java.util.Stack;
+
 public class LeftHand implements Iterator {
+    Stack<Mushroom> plate = new Stack<Mushroom>();
+
+    public void add(Mushroom mushroom)
+    {
+        plate.push(mushroom);
+    }
+    public boolean lastThreeMatches()
+    {
+        Mushroom m1 = plate.peek();
+        plate.pop();
+        Mushroom m2 = plate.peek();
+        plate.pop();
+        Mushroom m3 = plate.peek();
+        plate.push(m2);
+        plate.push(m1);
+
+        if(m1.getColor() == m2.getColor() && m2.getColor() == m3.getColor())
+            return true;
+        return false;
+    }
+
+    public Mushroom lastMushroom()
+    {
+        return plate.peek();
+    }
 }
